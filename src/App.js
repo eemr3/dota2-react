abeb8c4310406b0b387cb62b261901c0bb3d406b
Emerson <eemr3@yahoo.com.br>
2021-11-01 23:16:13 -0300

    build(App): Import dos componetes Header e Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
 import CardContainer from './components/CardContainer';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import './App.css';
 
 function App() {
 
   return (
-    <div className="App">
-     <CardContainer />
-    </div>
+    <>
+      <Header />
+      <CardContainer />
+      <Footer />
+    </>
   );
 }
 
