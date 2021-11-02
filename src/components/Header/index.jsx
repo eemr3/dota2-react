950e6124cb9526ad883825ed33172ae9a8bb8844
Emerson <eemr3@yahoo.com.br>
2021-11-01 23:11:46 -0300

    build(Header): Adiciona className para estilização

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import './Header.css'
 class Header extends Component {
+  
   render() {
     return (
-      <header>
+      <header className="dota__header">
         <h1>Cards Dota Gamming Online</h1>
       </header>
     );
