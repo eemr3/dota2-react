42852ce73e6e8e23f7e19e7cf8e317a3cee3bb09
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-11-01 22:24:03 -0300

    build(App): Feita implemtação do componente CardContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
 import './App.css';
+import CardContainer from './components/CardContainer';
 
 function App() {
 
   return (
     <div className="App">
-     <h1>Dota 2</h1>
+     <CardContainer />
     </div>
   );
 }
